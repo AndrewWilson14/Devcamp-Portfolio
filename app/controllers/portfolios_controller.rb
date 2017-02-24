58e31eb9537093d9afb6c10eaa82464776f4df45
Andrew Wilson <andrewwilson1414@gmail.com>
2017-02-24 14:48:40 -0700

    Added the destroy feature for portolio-feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,6 +41,17 @@ def create
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    #this is going to perform the lookupo
+    @portfolio_item = Portfolio.find(params[:id])
+    #this is going to desyroy/delete the record
+    @portfolio_item.destroy
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed!.' }
+    end
+  end
+
 end
 
 
