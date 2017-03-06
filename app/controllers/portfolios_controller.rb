5ef292ad335cd04546bd83cc84dc39adffa097fb
Andrew Wilson <andrewwilson1414@gmail.com>
2017-03-06 15:11:00 -0700

    implemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,6 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
-  before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
-
+  
   def index
     @portfolio_items = Portfolio.all
   end
@@ -15,7 +14,7 @@ class PortfoliosController < ApplicationController
     3.times { @portfolio_item.technologies.build }
   end
 
-def create
+  def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
@@ -28,41 +27,47 @@ def create
   end
 
   def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
+    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def show
-  end
-
-  def destroy
-    #this is going to desyroy/delete the record
-    @portfolio_item.destroy
-    # Redirect
-    respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Record was removed!.' }
-  end
-end
-
-  private
-  def set_portfolio
     @portfolio_item = Portfolio.find(params[:id])
   end
 
-  def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
 end
-
-
