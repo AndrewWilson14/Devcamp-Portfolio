3eb1760603b48c45509f0328146c31f7b0f5f94b
Andrew Wilson <andrewwilson1414@gmail.com>
2017-03-19 12:07:22 -0600

    Updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,10 @@ module ApplicationHelper
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to connect!"
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
@@ -72,7 +72,7 @@ module ApplicationHelper
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "Andrew Wilson Portfolio", sticky: false)
+    js add_gritter(msg, title: "Andrew Wilson's Portfolio", sticky: false)
   end
 
 end
