8859c78062126ccd6b3d735dc5d984fc71a57bf0
Andrew Wilson <andrewwilson1414@gmail.com>
2017-03-21 10:18:36 -0600

    fixed blogs_helper page

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -5,9 +5,9 @@ module BlogsHelper
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.scan(code, language ||= :ruby).div
     end
-  end
+  end 
 
   def markdown(text)
     coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
